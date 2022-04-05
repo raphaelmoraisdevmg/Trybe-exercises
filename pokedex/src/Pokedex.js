4f0656d45a671a2158113bff1a278aab0ba93e02
Raphael Almeida de Morais <raphaelmoraisdev@gmail.com>
2022-04-05 18:19:52 -0300

    exercicio finalizado

--- pokedex/src/Pokedex.js
+++ pokedex/src/Pokedex.js
@@ -6,7 +6,13 @@ class Pokedex extends React.Component{
     render(){
         return(
             <div>
-                <Pokemon />
+                { Data.map((pokemon) =>
+                <Pokemon 
+                name={pokemon.name}
+                type={pokemon.type}
+                weigh={`peso: ${pokemon.averageWeight.value}${pokemon.averageWeight.measurementUnit}`}
+                src={pokemon.image}    
+                />)}
             </div>
         );
     }
--- pokedex/src/Pokemon.js
+++ pokedex/src/Pokemon.js
@@ -2,9 +2,13 @@ import React  from "react";
 
 class Pokemon extends React.Component{
     render(){
+        const  {name,type,weigh,src} = this.props
         return(
             <div>
-                <h1>Nome do pokémon</h1>
+                <h1>{name}</h1>
+                <p>{type}</p>
+                <p>{weigh}</p>
+                <img src={src} alt={name} />
             </div>
         );
     }
